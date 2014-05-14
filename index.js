1d4592cd39d818e4d8f52b1de00435222e23e36a
Carlo De Gregorio <cdegregorio@gmail.com>
2014-05-14 15:11:52 -0700

    These are the first two passing tests

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // your function and tests should go in here
 
-describe('tests', function() {
-  it('needs real tests', function() {
-    expect(1).to.eql(1);
+var fallingTime = function(distance) {
+	return Math.sqrt((2*(distance)) / 9.81);	
+};
+
+
+describe('fallingTime()', function() {
+  it('calculates how long it takes an object to fall from height of 50 meters', function() {
+    expect(fallingTime(50)).to.be.closeTo(3.2, 0.01);
+  });
+
+  it('calculates how long it takes an object to fall from height of 1 meter', function() {
+    expect(fallingTime(1)).to.be.closeTo(0.45, 0.01);
   });
 });
+
