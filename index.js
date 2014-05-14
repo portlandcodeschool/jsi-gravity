a39f27ca0dcca64b5ab449b8340666b577458ca8
Sam Averett <averettsam@comcast.net>
2014-05-14 15:19:50 -0700

    two new tests added

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ describe('timeTheDrop()', function() {
   	'distance of 10 meters to within .1 sec', function() {
     expect(timeTheDrop(10)).to.be.closeTo(1.4, .1);
   });
+
   it('calculates the time of fall from 100 meters as 4.5', function() {
     expect(timeTheDrop(100)).to.be.closeTo(4.5, .02);
   });
+
+  it('reject a negative height', function() {
+    expect(timeTheDrop(-10)).to.be.NaN;
+  });
+
+  it('rejects a string as input', function() {
+    expect(timeTheDrop('abc')).to.eql(NaN);
+  });
+
 });
