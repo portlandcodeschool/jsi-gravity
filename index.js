7dcaef11cd9a88fd711b92d7d8051724a8758495
Tian Song <tian.y.song@gmail.com>
2014-05-14 15:18:58 -0700

    Two tests for calculating time fall

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // your function and tests should go in here
+var calculateFallTime = function (distance) {
+	return Math.sqrt((2 * distance) / 9.81);
+};
 
-describe('tests', function() {
-  it('needs real tests', function() {
-    expect(1).to.eql(1);
+
+describe('calculate fall time', function() {
+  it('should find out the time needed for an object' + 
+  	' to fall a given distance', function() {
+    expect(calculateFallTime(20)).to.be.closeTo(2.02, 0.005);
+  });
+  it('rejects negative distance', function() {
+  	expect(calculateFallTime(-1)).to.be.eql(NaN);
   });
 });
