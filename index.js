a57856f050e3180c570f208efdf90266bbc15a0b
Dmitry White <white.dmitry@gmail.com>
2014-05-14 15:19:55 -0700

    Added 2 more tests for negative height and string value entry

--- index.js
+++ index.js
@@ -13,9 +13,18 @@ describe('timeTheDrop()', function() {
   	'from 10 meters to within .1 seconds', function() {
     expect(timeTheDrop(10)).to.be.closeTo(1.4, .1);
   });
+
   it('calculates the time of fall from 100 meters ' +
   	'as 4.5 seconds to within .02 seconds', function() {
-  		expect(timeTheDrop(100)).to.be.closeTo(4.5, .02);
+  	expect(timeTheDrop(100)).to.be.closeTo(4.5, .02);
   	});
+
+  it('rejects a negative height', function() {
+  	expect(timeTheDrop(-10)).to.eql(NaN);
+  });
+
+  it('rejects a string', function() {
+  	expect(timeTheDrop("Smattamy")).to.eql(NaN);
+  })
   });
 
