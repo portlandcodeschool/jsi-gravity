c4e6513fad854def98f77c254512c92f50d2532d
Sam Averett <averettsam@comcast.net>
2014-05-14 14:59:06 -0700

    Basic functions work for two different distances

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // your function and tests should go in here
 
-describe('tests', function() {
-  it('needs real tests', function() {
-    expect(1).to.eql(1);
+var timeTheDrop = function(ht) {
+	var dropTime;
+	grav = 9.81;
+	preRoot = (2 * ht) / grav;
+	dropTime = Math.pow(preRoot, .5);
+	return dropTime;
+
+};
+
+describe('timeTheDrop()', function() {
+  it('calculate the time an object takes to fall a' + 
+  	'distance of 10 meters to within .1 sec', function() {
+    expect(timeTheDrop(10)).to.be.closeTo(1.4, .1);
+  });
+  it('calculates the time of fall from 100 meters as 4.5', function() {
+    expect(timeTheDrop(100)).to.be.closeTo(4.5, .02);
   });
 });
