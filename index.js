4498827b4caa11f66793ddcf53475fc7c5e05c74
Dmitry White <white.dmitry@gmail.com>
2014-05-14 15:39:08 -0700

    Cleaned up index file with JSHint

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ var timeTheDrop = function(ht) {
 	var dropTime;
 	grav = 9.81;
 	preRoot = (2 * ht) / grav;
-	dropTime = Math.pow(preRoot, .5);
+	dropTime = Math.pow(preRoot, 0.5);
 	return dropTime;
 };
 
 describe('timeTheDrop()', function() {
   it('correctly calculates the duration of a fall ' +
   	'from 10 meters to within .1 seconds', function() {
-    expect(timeTheDrop(10)).to.be.closeTo(1.4, .1);
+    expect(timeTheDrop(10)).to.be.closeTo(1.4, 0.1);
   });
 
   it('calculates the time of fall from 100 meters ' +
   	'as 4.5 seconds to within .02 seconds', function() {
-  	expect(timeTheDrop(100)).to.be.closeTo(4.5, .02);
+  	expect(timeTheDrop(100)).to.be.closeTo(4.5, 0.02);
   	});
 
   it('rejects a negative height', function() {
@@ -25,6 +25,6 @@ describe('timeTheDrop()', function() {
 
   it('rejects a string', function() {
   	expect(timeTheDrop("Smattamy")).to.eql(NaN);
-  })
+  });
   });
 
