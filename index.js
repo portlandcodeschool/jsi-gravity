60fa498e9218312dff23e697a428404608b6c462
Grant Stampfli <mail@gstampfli.com>
2014-05-14 15:27:22 -0700

    built a 2nd test

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ describe('caculateTime()', function () {
 	it('caculates the time it takes for an object to fall 30 meters', function () {
 		expect(caculateTime(30)).to.be.closeTo(2.4731, 0.0001);
 	});
-	it('rejects a negative a distance.');
+	it('rejects a negative a distance.', function () {
+		expect(caculateTime(-30)).to.be.NaN;
+	});
 	it('it returns a distance of 0.');
 });
