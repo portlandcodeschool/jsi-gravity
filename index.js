a08ce2b41baab591e2555826c272cf0191e3f41b
Grant Stampfli <mail@gstampfli.com>
2014-05-14 15:14:10 -0700

    fixed travis failings

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var caculateTime = function (distance){
 	var base = (2 * distance / 9.81);
 	return Math.pow(base, 0.5);
-}
+};
 describe('caculateTime()', function () {
 	it('caculates the time it takes for an object to fall 30 meters', function () {
-		expect(caculateTime(30)).to.be.closeTo(2.4731, .0001);
+		expect(caculateTime(30)).to.be.closeTo(2.4731, 0.0001);
 	});
 	it('rejects a negative a distance.');
-	it('it returns a distance of 0.')
-})
+	it('it returns a distance of 0.');
+});
