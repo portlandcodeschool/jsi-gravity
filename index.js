f7c5f3075905b0cce4c271f06069b533ae40224d
Kali Kiger <kalikiger@gmail.com>
2014-09-05 14:03:30 -0700

    added new gravity tests to index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// your function and tests should go in here
+/**
+* Calculate interest
+* @param {distance} Will return unexpected results if not entered in meters
+*/
 
-describe('tests', function() {
-  it('needs real tests', function() {
-    expect(1).to.eql(1);
+
+var fallingTime = function (distance) {
+	return parseFloat((Math.sqrt(((2*distance))/9.8)).toFixed(1)); 
+};
+
+describe('time', function() {
+  it('calculates time it will take to fall 10 meters', function() {
+    var time = fallingTime(10);
+    expect(time).to.eql(1.4);
   });
 });
