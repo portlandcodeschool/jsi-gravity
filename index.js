bb1caaccda6e6a909eaee0f4f1c6439bd3ce5773
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-05 14:06:34 -0700

    Created falling calculator and test.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-// your function and tests should go in here
+/**
+ * Calculate time falling
+ *
+ * @param {number} the distance inputed
+ * @param {number} constant speed of falling 54m/s
+ * @return {number} time falling close to
+ */
 
-describe('tests', function() {
-  it('needs real tests', function() {
-    expect(1).to.eql(1);
+var calculateFalling = function (distance) {
+	var gravity = 9.81;
+	var insert = (2*distance/gravity);
+	var fallingTime = Math.sqrt(insert);
+	return fallingTime;
+} 
+
+var test = calculateFalling(54);
+console.log(test);
+
+describe('calculateFalling()', function() {
+  it('calculates time object takes to fall specific distance of 54 meters', function() {
+    var fallingTime = calculateFalling(54);
+    expect(fallingTime).to.be.closeTo(3.3,0.5);
+  });
+
+  it('calculates time object takes to fall specific distance of 600 meters', function() {
+    var fallingTime = calculateFalling(600);
+    expect(fallingTime).to.be.closeTo(11,0.5);
   });
 });
