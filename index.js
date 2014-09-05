25260b566fda523c69d84776d56280113ca85de9
Austin Trafficante <austin.trafficante@gmail.com>
2014-09-05 12:07:00 -0700

    Completed a TDD challenge exercise

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // your function and tests should go in here
 
-describe('tests', function() {
-  it('needs real tests', function() {
-    expect(1).to.eql(1);
+/*
+ * @param {number} some distance in meters
+ * @return {number} the time it takes to travel said distance
+ */
+
+describe('gravity function', function() {
+  it('calculates the time it takes for an object to fall 10m', function() {
+    expect(gravity(10)).to.eql(1.43);
   });
 });
+
+var gravity = function(distance) {
+  // Some fancy physics equation to determine the time it takes to travel some distance
+  var g = 9.8; // Gravitational constant in m/s^2
+  return parseFloat(Math.sqrt(2 * distance / g).toFixed(2));
+};
+
+gravity(10);
