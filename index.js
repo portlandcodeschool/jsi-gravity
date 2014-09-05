72a4fa55dbf60e410b648cd5a6b6c53ffaa4560e
Stephen Young <syoung187@gmail.com>
2014-09-05 14:03:34 -0700

    made changes to index document with Kali

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // your function and tests should go in here
 
-describe('tests', function() {
-  it('needs real tests', function() {
-    expect(1).to.eql(1);
+// describe('tests', function() {
+//   it('needs real tests', function() {
+//     expect(1).to.eql(1);
+//   });
+// });
+
+var fallingTime = function (distance) {
+	return parseFloat((Math.sqrt(((2*10))/9.8)).toFixed(1));
+};
+
+describe('time', function() {
+  it('calculates the time it will take to fall 10 feet', function() {
+  	var time = fallingTime(10);
+    expect(time).to.eql(1.4);
   });
 });
+
